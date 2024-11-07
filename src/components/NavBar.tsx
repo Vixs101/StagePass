bf4ee865b901b81403da0a5c9fd46b92178468dd
Vixs101 <elijahvix695@gmail.com>
2024-11-07 15:41:30 +0100

    Feature: started working on the navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <nav className='mx-28 bg-gray-50'>
+        <img src="/images/logo.png" alt="logo" />
+    </nav>
+  )
+}
