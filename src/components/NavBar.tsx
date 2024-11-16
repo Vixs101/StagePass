ee6509eb84498a9293db11308fc25e287f6996d9
Vixs101 <elijahvix695@gmail.com>
2024-11-16 17:47:41 +0100

    feature:finished the homepage and navbar responsiveness

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,27 +1,102 @@
-// import React from 'react'
+"use client";
 
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Menu, X, Home, Calendar, Ticket, LogIn } from "lucide-react";
 
 export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="fixed w-full flex justify-between items-center px-28 py-3 bg-gray-50 z-30">
-      <Link to="/">
-        <img src="/images/logo.png" alt="logo" />
-      </Link>
-      <ul className="flex items-center gap-7 py-2 font-semibold text-gray-800 text-xl">
-        <li>
-          <Link to="/" className="hover:border-b-2 hover:border-b-[#4bf44d]">Home</Link>
-        </li>
-        <li>
-          <Link to="/events" className="hover:border-b-2 hover:border-b-[#4bf44d]">Events</Link>
-        </li>
-        <li>
-          <Link to="/tickets" className="hover:border-b-2 hover:border-b-[#4bf44d]">Tickets</Link>
-        </li>
-        <button className="bg-gray-800 py-2 px-3 rounded-sm text-white hover:bg-gray-700">
-          Login
-        </button>
-      </ul>
+    <nav className="fixed w-full bg-gray-50 z-10">
+      <div className="px-4 sm:px-6 md:px-16 xl:px-28">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <Link to="/">
+              <img src="/images/logo.png" alt="logo" className="" />
+            </Link>
+          </div>
+          <div className="hidden md:block">
+            <ul className="ml-10 flex items-baseline space-x-4 text-xl font-semibold">
+              <li>
+                <Link
+                  to="/"
+                  className="hover:border-b-2 hover:border-b-[#4bf44d] px-3 py-2 rounded-md font-medium text-gray-800"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/events"
+                  className="hover:border-b-2 hover:border-b-[#4bf44d] px-3 py-2 rounded-md font-medium text-gray-800"
+                >
+                  Events
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/tickets"
+                  className="hover:border-b-2 hover:border-b-[#4bf44d] px-3 py-2 rounded-md font-medium text-gray-800"
+                >
+                  Tickets
+                </Link>
+              </li>
+              <button className="bg-gray-800 py-2 px-3 rounded-md text-sm font-medium text-white hover:bg-gray-700">
+                Login
+              </button>
+            </ul>
+          </div>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="inline-flex items-center justify-center p-5 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {isOpen ? (
+                <X className="block h-8 w-8 text-gray-800" />
+              ) : (
+                <Menu className="block h-8 w-8 text-gray-800" />
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className="md:hidden" id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link
+              to="/"
+              className="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium text-gray-800"
+            >
+              <Home className="inline-block mr-2" size={18} />
+              Home
+            </Link>
+            <Link
+              to="/events"
+              className="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium text-gray-800"
+            >
+              <Calendar className="inline-block mr-2" size={18} />
+              Events
+            </Link>
+            <Link
+              to="/tickets"
+              className="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium text-gray-800"
+            >
+              <Ticket className="inline-block mr-2" size={18} />
+              Tickets
+            </Link>
+            <button className="flex items-center text-left  bg-gray-800 hover:bg-gray-700 text-white px-3 py-2 rounded-md text-base font-medium w-1/2">
+              <LogIn className="inline-block mr-2" size={18} />
+              Login
+            </button>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,52 +5,52 @@ import { GiJusticeStar } from "react-icons/gi";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-20 bg-gradient-to-br from-white via-[#4bf44d] to-white">
+    <div className="flex flex-col gap-10 md:gap-20 bg-gradient-to-br from-white via-[#4bf44d] to-white">
       <NavBar />
       {/* hero section */}
-      <section className="relative h-screen flex gap-8 px-28 pt-36">
-        <div className="z-20 flex flex-col gap-16 pt-20 w-1/2">
+      <section className="xl:h-screen flex xl:flex-row flex-col justify-between gap-8 md:gap-5 xl:gap-8 p-5 md:px-16 xl:px-28 md:pt-20 xl:pt-36">
+        <div className="flex flex-col gap-6 md:gap-16 pt-20 text-center md:text-start xl:w-1/2">
           <div className="flex gap-5 flex-col">
-            <h1 className="text-5xl font-semibold">
+            <h1 className="text-3xl md:text-5xl font-semibold">
               Experience The Magic of Live Theatre
             </h1>
-            <p className="text-2xl">
+            <p className="text-lg md:text-2xl">
               Immerse yourself in the world of dance, musicals, drama, and
               poetry with StagePass. Explore our upcoming events today and
               secure your tickets for unforgettable performances.
             </p>
           </div>
-          <button className="bg-gray-800 p-4 rounded-sm text-white hover:bg-gray-700 w-1/4">
+          <button className="bg-gray-800 p-4 text-white hover:bg-gray-700 w-full md:w-1/2 xl:w-1/4 rounded-md place-self-center xl:place-self-start">
             Explore
           </button>
         </div>
         {/* fixed background image */}
-        <img src="/images/heroImage.webp" alt="hero Image" className="w-5/12 h-4/5" />
+        <img src="/images/heroImage.webp" alt="hero Image" className="xl:w-5/12 md:h-4/5 rounded-md" />
       </section>
       {/* schedule */}
-      <section className="flex flex-col gap-10 px-28 h-screen">
-        <div className="flex flex-col gap-5">
-          <h1 className="text-5xl font-semibold">Event Schedule</h1>
-          <p className="text-xl">Plan your visit</p>
+      <section className="flex flex-col gap-10 p-5 md:px-28 xl:h-screen">
+        <div className="flex flex-col gap-5 text-center md:text-start">
+          <h1 className="text-3xl md:text-5xl font-semibold">Event Schedule</h1>
+          <p className="text-lg md:text-xl">Plan your visit</p>
         </div>
         <hr className="h-1 bg-gray-800" />
-        <p className="text-3xl w-3/4">
+        <p className="text-lg text-center md:text-start md:text-3xl md:w-3/4">
           Check out our event schedule to see when your favorite performances
           are taking place. Be sure to mark your calendar and join us for an
           evening filled with entertainment.{" "}
         </p>
         <hr className="h-1 bg-gray-800 rounded-lg" />
-        <div className="flex flex-col pl-32 text-2xl">
+        <div className="flex flex-col md:pl-32 text-xl md:text-2xl">
           {/* first section */}
-          <div className="flex gap-10 mb-6">
-            <div className="flex gap-14 w-5/12">
-              <GiJusticeStar size={50} />
-              <div className="flex flex-col gap-7">
+          <div className="flex md:flex-row flex-col gap-10 mb-6 ">
+            <div className="flex gap-5 md:gap-14 md:w-5/12">
+              <GiJusticeStar size={50} className="" />
+              <div className="flex flex-col gap-3 md:gap-7">
                 <h3>Dance</h3>
                 <p>Expressive Movements</p>
               </div>
             </div>
-            <p className="mt-2 w-2/3">
+            <p className="mt-2 ml-20 md:ml-0 md:w-2/3">
               From contemporary to traditional dances, our performances showcase
               a range of expressive movements that captivate audiences and tell
               compelling stories through choreography.
@@ -58,15 +58,15 @@ export default function Home() {
           </div>
           <hr className="h-1  bg-gray-800 rounded-lg" />
           {/* second section */}
-          <div className="flex gap-10 my-6">
-            <div className="flex gap-14 w-5/12">
+          <div className="flex md:flex-row flex-col gap-10 mb-6 md:mt-5">
+            <div className="flex gap-5 md:gap-14 md:w-5/12 mt-12 md:mt-0">
               <TiStarburst size={50} />
-              <div className="flex flex-col gap-7">
+              <div className="flex flex-col gap-3 md:gap-7">
                 <h3>Musicals</h3>
                 <p>Harmonious Melodies</p>
               </div>
             </div>
-            <p className="mt-2 w-2/3">
+            <p className="mt-2 ml-20 md:ml-0 md:w-2/3">
               Experience the magic of live musicals with our talented cast and
               crew. Let the harmonious melodies, captivating lyrics, and
               dazzling performances transport you to different worlds.
@@ -74,15 +74,15 @@ export default function Home() {
           </div>
           <hr className="h-1 bg-gray-800 rounded-lg" />
           {/* third section */}
-          <div className="flex gap-10 mt-6">
-            <div className="flex gap-14 w-5/12">
+          <div className="flex md:flex-row flex-col gap-10 mb-6 md:mt-5">
+            <div className="flex gap-5 md:gap-14 md:w-5/12 mt-12 md:mt-0">
               <GiPolarStar size={50} />
-              <div className="flex flex-col gap-7">
+              <div className="flex flex-col gap-3 md:gap-7">
                 <h3>Drama</h3>
                 <p>Intense Narratives</p>
               </div>
             </div>
-            <p className="mt-2 w-2/3">
+            <p className="mt-2 ml-20 md:ml-0 md:w-2/3">
               Get ready for gripping storytelling and powerful acting in our
               dramatic presentations. Explore complex narratives that delve into
               the depths of human emotions and experiences..
@@ -90,10 +90,10 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <hr className="h-1 bg-gray-800 mx-28 rounded-lg" />
-      <footer className="flex justify-between px-28 py-8">
-        <h1 className="text-3xl">StagePass</h1>
-        <div className="flex gap-20 text-2xl px-10">
+      <hr className="h-1 bg-gray-800 mx-5 md:mx-28 rounded-lg" />
+      <footer className="flex md:flex-row flex-col md:justify-between px-10 pb-12 md:px-28 md:py-8 gap-10 md:gap-0">
+        <h1 className="font-semibold md:font-normal text-3xl">StagePass</h1>
+        <div className="flex flex-col md:flex-row gap-5 md:gap-20 text-2xl md:px-10">
           <div>
             <p>+234-333-333</p>
             <p>info@mysite.com</p>
